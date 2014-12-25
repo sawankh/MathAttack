44e2747bb4280f3b6fe87137e15be31538af4411
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-25 14:46:05 +0000

    First commit raw project ready to start

---
+++ android/src/com/sawan/mathattack/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sawan.mathattack.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sawan.mathattack.MAGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MAGame(), config);
+	}
+}
---
+++ desktop/src/com/sawan/mathattack/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sawan.mathattack.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sawan.mathattack.MAGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MAGame(), config);
+	}
+}
