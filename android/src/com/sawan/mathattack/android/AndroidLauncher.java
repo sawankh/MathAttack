e03766a84e25634c11531866a9df92e247789eab
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-08 14:23:03 +0000

    changed mainstarter in android project

--- android/src/com/sawan/mathattack/android/AndroidLauncher.java
+++ android/src/com/sawan/mathattack/android/AndroidLauncher.java
@@ -29,12 +29,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sawan.mathattack.MainStarter;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new com.sawan.jungle.MainStarter(), config);
+		initialize(new MainStarter(), config);
 	}
 }
