4295c705ed64825bea9b60c923b484661f06a1f8
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-14 21:42:30 +0000

    Added levelTable class

---
+++ core/src/com/sawan/mathattack/game_screens/levels/helpers/MALevelTable.java
@@ -0,0 +1,34 @@
+/**
+ * File name:	MALevelTable.java
+ * Version:		1.0
+ * Date:		14/03/2015 21:41:58
+ * Author:		Sawan
+ * Copyright:	Copyright 200X Sawan
+ *
+ *				This file is part of Math Attack.
+ *
+ *				Math Attack is free software: you can redistribute it 
+ *				and/or modify it under the terms of the GNU General
+ *				Public License as published by the Free Software 
+ *				Foundation, either version 3 of the License, 
+ *				or (at your option) any later version.
+ *
+ *				Math Attack is distributed in the hope that it will 
+ *				be useful, but WITHOUT ANY WARRANTY; without even 
+ *				the implied warranty of MERCHANTABILITY or FITNESS 
+ *				FOR A PARTICULAR PURPOSE. See the GNU General Public
+ *			    License for more details.
+ *
+ *				You should have received a copy of the GNU General 
+ *				Public License along with Math Attack. If not, see 
+ *				http://www.gnu.org/licenses/.
+ */
+package com.sawan.mathattack.game_screens.levels.helpers;
+
+/**
+ * @author Sawan
+ *
+ */
+public class MALevelTable {
+
+}
