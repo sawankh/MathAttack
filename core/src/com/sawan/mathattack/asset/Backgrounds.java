3b332d65a9e834fc81f333a138461f3250430ba2
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-15 16:21:46 +0000

    Fixed background string

--- core/src/com/sawan/mathattack/asset/Backgrounds.java
+++ core/src/com/sawan/mathattack/asset/Backgrounds.java
@@ -123,7 +123,7 @@ public class Backgrounds {
 	 */
 	public static void loadImages() {
 		image_main_background = getAtlas().findRegion("menu_background");
-		image_level_background = getAtlas().findRegion("backgrounds2");
+		image_level_background = getAtlas().findRegion("backgrounds2.png");
 	}
 
 	/**
