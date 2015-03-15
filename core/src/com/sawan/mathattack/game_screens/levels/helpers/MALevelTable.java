00a0b786560b0902998051cbf13a8ff272d8c86d
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-15 15:55:07 +0000

    Completed level table code

--- core/src/com/sawan/mathattack/game_screens/levels/helpers/MALevelTable.java
+++ core/src/com/sawan/mathattack/game_screens/levels/helpers/MALevelTable.java
@@ -25,22 +25,43 @@
  */
 package com.sawan.mathattack.game_screens.levels.helpers;
 
+import java.util.Random;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.sawan.mathattack.asset.UIAssets;
+import com.sawan.mathattack.buttons.ButtonLevel;
 import com.sawan.mathattack.game_screens.levels.MALevelScreen;
 import com.sawan.mathattack.scene2d.ui.MenuCreator;
 
 /**
- * @author Sawan
+ * The Class MALevelTable.
  *
+ * @author Sawan
  */
 public class MALevelTable {
 
+	/** The Constant PADDING_VALUE. */
 	private final static int PADDING_VALUE = 30;
 	
+	/** The Constant NUM_LEVELS. */
 	private final static int NUM_LEVELS = 8;
 	
+	/** The Constant NUM_MAX_STARS. */
+	private final static int NUM_MAX_STARS = 3;
+	
+	/** The Constant MAX_LEVELS_PER_ROW. */
+	private final static int MAX_LEVELS_PER_ROW = 4;
+	
+	/**
+	 * Level table.
+	 *
+	 * @param levelScreen the level screen
+	 */
+	@SuppressWarnings("unused")
 	private void levelTable(final MALevelScreen levelScreen) {
 		levelScreen.level_table = MenuCreator.createTable(true, UIAssets.getSkin());
 		levelScreen.level_table.setPosition(-999, 0);
@@ -48,9 +69,35 @@ public class MALevelTable {
 		levelScreen.level_table.top().left().pad(PADDING_VALUE, PADDING_VALUE, PADDING_VALUE, PADDING_VALUE);
 		levelScreen.getStage().addActor(levelScreen.level_table);
 		
-		for (int i = 0; i < NUM_LEVELS; i++) {
-			Drawable drawable_up;
-			Drawable drawable_down;
+		for (int level_number = 0; level_number < NUM_LEVELS; level_number++) {
+			// Create the button
+			Drawable drawable_up = new TextureRegionDrawable(UIAssets.button_level);
+			Drawable drawable_down = new TextureRegionDrawable(UIAssets.button_level);
+			final ButtonLevel button_level = new ButtonLevel(drawable_up, drawable_down);
+			
+			// Set button level
+			button_level.setLevel_number(level_number, UIAssets.cartwheel_font);
+			
+			// Set stars (get from database or text files here)
+			// Random number of stars just for testing 
+			Random random =  new Random();
+			button_level.setLevelStars(UIAssets.image_level_no_star, UIAssets.image_level_star, NUM_MAX_STARS, random.nextInt(NUM_MAX_STARS) + 1);
+			
+			// Add listener
+			button_level.addListener(new ActorGestureListener() {
+				@Override
+				public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+					super.touchUp(event, x, y, pointer, button);
+				}
+			});
+
+			// Limit max number of levels per row to 4
+			if (level_number % MAX_LEVELS_PER_ROW == 0) {
+				levelScreen.level_table.row();
+			}
+			
+			// Add to table
+			levelScreen.level_table.add(button_level).size(50, 50).pad(5, 5, 5, 5).expand();
 		}
 		
 	}
