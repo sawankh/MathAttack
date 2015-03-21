6ca4ec13fe5848280c5da88aaba0356c8c569956
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-21 19:09:12 +0000

    Added enemies to test core

--- core/src/com/sawan/mathattack/game/layers/WorldLayerActors.java
+++ core/src/com/sawan/mathattack/game/layers/WorldLayerActors.java
@@ -25,9 +25,13 @@
  */
 package com.sawan.mathattack.game.layers;
 
+import java.util.ArrayList;
+
+import com.sawan.mathattack.asset.BlueMonsterAssets;
 import com.sawan.mathattack.asset.HeroAssests;
 import com.sawan.mathattack.game.managers.MAGameManager;
 import com.sawan.mathattack.models.characters.Hero;
+import com.sawan.mathattack.models.characters.enemies.BlueMonster;
 import com.sawan.mathattack.scene2d.AbstractWorldScene2d;
 import com.sawan.mathattack.settings.AppSettings;
 
@@ -39,11 +43,15 @@ public class WorldLayerActors extends AbstractWorldScene2d {
 	
 	private MAGameManager gameManager;
 	private Hero hero;
+	private ArrayList<BlueMonster> enemies;
+	
+	protected final static int NUM_ENEMIES = 20;
 	
 	public WorldLayerActors(MAGameManager gameManager, float posX, float posY, float worldWidth, float worldHeight) {
 		super(posX, posY, worldWidth, worldHeight);
 		this.gameManager =  gameManager;
 		setUpHero();
+		setUpEnemies();
 	}
 	
 	public void setUpHero() {
@@ -54,5 +62,24 @@ public class WorldLayerActors extends AbstractWorldScene2d {
 		hero.setX(0f * AppSettings.getWorldPositionXRatio());
 		hero.setAnimation(HeroAssests.hero_faint, true, true);
 		addActor(hero);
+	}
+	
+	public void setUpEnemies() {
+		enemies = new ArrayList<BlueMonster>();
+		
+		for (int i = 0; i < NUM_ENEMIES; i++) {
+			BlueMonster current_monster = new BlueMonster(gameManager.worldLayer_background.SOIL_WIDHT, gameManager.worldLayer_background.SOIL_HEIGHT, true);
+			
+			float posY = gameManager.worldLayer_background.SOIL_HEIGHT * AppSettings.getWorldSizeRatio();
+			current_monster.setY(posY);
+			current_monster.setX(gameManager.getStage().getWidth() + 200);
+			
+			current_monster.setAnimation(BlueMonsterAssets.monster_walking, true, true);
+			
+			current_monster.startMoving(gameManager.getStage().getWidth(), 110, true);
+			
+			enemies.add(current_monster);
+			addActor(current_monster);
+		}
 	}
 }
