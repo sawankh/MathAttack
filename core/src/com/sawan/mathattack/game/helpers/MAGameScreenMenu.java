48ca823fd8e49aa2235b4046627c2c96045d9ed9
Sawan J. Kapai Harpalani <sawanjagdish@itop.es>
2015-03-26 08:36:01 +0000

    Need to fix background orientation

--- core/src/com/sawan/mathattack/game/helpers/MAGameScreenMenu.java
+++ core/src/com/sawan/mathattack/game/helpers/MAGameScreenMenu.java
@@ -80,10 +80,8 @@ public class MAGameScreenMenu {
 		
 		quiz_table.setPosition(gameScreen.getStage().getWidth() / 2 - (quiz_table.getWidth() / 2), gameScreen.getStage().getHeight() - quiz_table.getHeight());
 		
-		Drawable background_table =  new TextureRegionDrawable(UIAssets.image_empty_bg);
-		
-		quiz_table.rotate(90f);
-		quiz_table.setBackground(background_table);
+		//Drawable background_table =  new TextureRegionDrawable(UIAssets.image_empty_bg);
+		//quiz_table.setBackground(background_table);
 		
 		
 		gameScreen.getStage().addActor(quiz_table);
