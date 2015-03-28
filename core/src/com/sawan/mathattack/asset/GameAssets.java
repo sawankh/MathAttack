260b082fee8562c204f92a1b7759863bd6b2bdae
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-03-28 16:33:12 +0000

    Loaded assets in class

--- core/src/com/sawan/mathattack/asset/GameAssets.java
+++ core/src/com/sawan/mathattack/asset/GameAssets.java
@@ -25,10 +25,139 @@
  */
 package com.sawan.mathattack.asset;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.sawan.mathattack.animation.AnimationCreator;
+
 /**
  * @author Sawan
  *
  */
 public class GameAssets {
+	
+	/** The Constant FILE_IMAGE_ATLAS. */
+	private final static String FILE_IMAGE_ATLAS = "data/ma/game/game/Game_assets.atlas";
+	
+	/** The Constant FILE_UI_SKIN. */
+	private final static String FILE_UI_SKIN = "skin/uiskin.json";
+	
+	/** The atlas. */
+	public static TextureAtlas atlas;
+	
+	/** The skin. */
+	public static Skin skin;
+	
+	// Assets
+	public static Animation heart;
+	public static TextureRegion eraser;
+	public static TextureRegion scissors;
+	public static TextureRegion pencil;
+	public static TextureRegion ruler;
+	public static TextureRegion semicircle;
+	public static TextureRegion trianguler;
+	public static TextureRegion pause;
+	
+	/**
+	 * Loads texture file.
+	 *
+	 * @param file the file
+	 * @return the texture
+	 */
+	public static Texture loadTexture(String file) {
+		return new Texture(Gdx.files.internal(file));
+	}
+	
+	/**
+	 * Gets the atlas. Creates a texture atlas if is not created. 
+	 *
+	 * @return the atlas
+	 */
+	public static TextureAtlas getAtlas() {
+		if (atlas == null) { 
+			atlas =  new TextureAtlas(Gdx.files.internal(FILE_IMAGE_ATLAS));
+		}
+	
+		return atlas;
+	}
+	
+	/**
+	 * Gets the skin.
+	 *
+	 * @return the skin
+	 */
+	public static Skin getSkin() {
+		if (skin == null) {
+			FileHandle skinFile = Gdx.files.internal(FILE_UI_SKIN);
+			skin = new Skin(skinFile);
+		}
+		
+		return skin;
+	}
+	
+	/**
+	 * Load all. Loads all the resources.
+	 */
+	public static void loadAll() {
+		relaseResources();
+		loadImages();
+		loadButtons();
+		loadFonts();
+		loadAnimations();
+		loadSoundsAndMusics();
+	}
 
+	/**
+	 * Relase resources.
+	 */
+	private static void relaseResources() {
+		skin = null;
+		atlas = null;
+	}
+
+	/**
+	 * Load images.
+	 */
+	public static void loadImages() {
+		eraser = getAtlas().findRegion("eraser");
+		pencil = getAtlas().findRegion("pencil");
+		ruler = getAtlas().findRegion("ruler");
+		scissors = getAtlas().findRegion("scissors");
+		trianguler = getAtlas().findRegion("trianguler");
+		semicircle = getAtlas().findRegion("semicircle");
+	}
+
+	/**
+	 * Load buttons.
+	 */
+	public static void loadButtons() {
+		pause = getAtlas().findRegion("pause");
+	}
+
+	/**
+	 * Load fonts.
+	 */
+	public static void loadFonts() {
+	
+	}
+
+	/**
+	 * Load animations.
+	 */
+	public static void loadAnimations() {
+		heart = AnimationCreator.getAnimationFromMultiTextures(getAtlas(), "heart", 8, 0.3f, false, false);
+	}
+
+	/**
+	 * Load sounds and musics.
+	 */
+	public static void loadSoundsAndMusics() {
+	
+	}
+	
+	
 }
